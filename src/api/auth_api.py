2ce0f7f01a11ae35de964b52a4030b688fb62b4c
Mohammed Saqib <mohammedsaqib532@gmail.com>
2026-02-09 07:44:13 +0530

    updating auth api file

--- src/api/auth_api.py
+++ src/api/auth_api.py
@@ -1,14 +1,15 @@
-from api.base_client import BaseAPIClient
+from src.api.base_client import BaseAPIClient
+
 
 class AuthAPI(BaseAPIClient):
-    def __init__(self):
-        super().__init__(
-            base_url="https://reqres.in"
-        )
+    LOGIN_ENDPOINT = "/login"
 
-    def login(self, email, password):
-        payload = {
-            "email": email,
-            "password": password
-        }
-        return self.post("/api/login", json=payload)
+    def login(self, email=None, password=None):
+        payload = {}
+
+        if email is not None:
+            payload["email"] = email
+        if password is not None:
+            payload["password"] = password
+
+        return self.post(self.LOGIN_ENDPOINT, json=payload)
