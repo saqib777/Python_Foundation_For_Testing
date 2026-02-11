d6cf63e77f53871519566e3b81ec2c85020c611c
Mohammed Saqib <mohammedsaqib532@gmail.com>
2026-02-11 15:15:14 +0530

    Adding test file - test_users_api

---
+++ tests/api/test_users_api.py
@@ -0,0 +1,37 @@
+import pytest
+from api.users_api import UsersAPI
+from api.validators import validate_user_object
+
+pytestmark = pytest.mark.api
+
+
+@pytest.fixture
+def users_api():
+    return UsersAPI()
+
+
+def test_get_users_success(users_api):
+    response = users_api.get_users(page=2)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    assert "data" in body
+    assert isinstance(body["data"], list)
+    assert len(body["data"]) > 0
+
+
+def test_get_single_user(users_api):
+    response = users_api.get_user_by_id(2)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    assert "data" in body
+    validate_user_object(body["data"])
+
+
+def test_get_user_not_found(users_api):
+    response = users_api.get_user_by_id(9999)
+
+    assert response.status_code == 404
